5164a0f40843367e40dece6223a19af1df29aa9e
Sean Dewar <seandewar@users.noreply.github.com>
2022-03-03 14:48:23 +0000

    feat: basic level initialization

--- lua/killersheep.lua
+++ lua/killersheep.lua
@@ -5,18 +5,47 @@ local loop = vim.loop
 
 local ns = api.nvim_create_namespace "killersheep"
 
-api.nvim_set_hl(0, "SheepTitle", { cterm = { bold = true }, bold = true })
-api.nvim_set_hl(0, "IntroHl", { ctermbg = "cyan", bg = "cyan" })
-api.nvim_set_hl(0, "KillerLevel", { ctermbg = "magenta", bg = "magenta" })
-api.nvim_set_hl(0, "KillerLevelX", { ctermbg = "yellow", bg = "yellow" })
+local HIGHLIGHTS = {
+  SheepTitle = { cterm = { bold = true }, bold = true },
+  introHl = { ctermbg = "cyan", bg = "cyan" },
+
+  KillerCannon = { ctermbg = "blue", bg = "blue" },
+  KillerBullet = { ctermbg = "red", bg = "red" },
+  KillerSheep = {
+    ctermfg = "black",
+    fg = "black",
+    ctermbg = "green",
+    bg = "green",
+  },
+  KillerSheep2 = {
+    ctermfg = "black",
+    fg = "black",
+    ctermbg = "cyan",
+    bg = "cyan",
+  },
+  KillerPoop = vim.o.background == "light"
+      and { ctermbg = "black", bg = "black" }
+    or { ctermbg = "white", bg = "white" },
+  KillerLevel = { ctermbg = "magenta", bg = "magenta" },
+  KillerLevelX = { ctermbg = "yellow", bg = "yellow" },
+}
+for hl, attrs in pairs(HIGHLIGHTS) do
+  attrs.default = true
+  api.nvim_set_hl(0, hl, attrs)
+end
+
+local function script_path()
+  return fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":p:h:h")
+end
+local SOUND_DIR = script_path() .. "/sound"
 
 local SOUND_PROVIDERS = {
   afplay = { cmd = { "afplay" }, ext = ".mp3" },
   paplay = { cmd = { "paplay" }, ext = ".ogg" },
   cvlc = { cmd = { "cvlc", "--play-and-exit" }, ext = ".ogg" },
 }
-local sound_provider
 
+local sound_provider
 local function detect_sound_provider()
   sound_provider = nil
   for exe, provider in pairs(SOUND_PROVIDERS) do
@@ -33,11 +62,6 @@ local function detect_sound_provider()
   }
   api.nvim_echo(chunks, true, {})
 end
-
-local function script_path()
-  return fn.fnamemodify(debug.getinfo(1, "S").source:sub(2), ":p:h:h")
-end
-local SOUND_DIR = script_path() .. "/sound"
 
 local music_job
 local function stop_music()
@@ -100,25 +124,102 @@ local function open_float(lines, config, on_close)
   })
   local win = api.nvim_open_win(buf, true, config)
 
-  local augroup_name = "killersheep.float" .. win
-  local augroup = api.nvim_create_augroup(augroup_name, {})
-  api.nvim_create_autocmd({ "WinLeave", "BufLeave", "VimLeavePre" }, {
-    group = augroup_name,
-    once = true,
-    buffer = buf,
-    callback = on_close,
-  })
+  local augroup
+  if on_close then
+    local augroup_name = "killersheep.float" .. win
+    augroup = api.nvim_create_augroup(augroup_name, {})
+    api.nvim_create_autocmd({ "WinLeave", "BufLeave", "VimLeavePre" }, {
+      group = augroup_name,
+      once = true,
+      buffer = buf,
+      callback = on_close,
+    })
+  end
 
   return buf, win, augroup
 end
 
-local function round(num)
-  -- TODO
+local SHEEP_SPRITES = {
+  {
+    " o^^) /^^^^^^\\",
+    "==___         |",
+    "     \\  ___  _/",
+    "      ||   ||",
+  },
+  {
+    " o^^) /^^^^^^\\",
+    "==___         |",
+    "     \\_ ____ _/",
+    "       |    |",
+  },
+  {
+    " o^^) /^^^^^^\\",
+    "==___         |",
+    "     \\  ___  _/",
+    "      ||   ||",
+  },
+  {
+    " o^^) /^^^^^^\\",
+    "==___         |",
+    "     \\ _ __ _ /",
+    "      / |  / |",
+  },
+  {
+    "        /^^^^^^\\",
+    "       |        |",
+    " O^^)            ",
+    "xx___ _         |",
+    "      \\ _____  _/",
+    "       ||    ||",
+  },
+  {
+    "         /^^^^^^\\",
+    "        |        |",
+    "                  ",
+    " O^^)             ",
+    "XX___             ",
+    "       \\ __  _  _/",
+    "        ||    ||",
+  },
+}
+
+local function level(num)
+  local _, level_win = open_float({ " Level " .. num .. " " }, { row = 0 })
+  vim.wo[level_win].winhighlight =
+    "NormalFloat:KillerLevel,FloatBorder:KillerLevel"
+
+  local cannon_win
+  _, cannon_win = open_float(
+    { "  /#\\", " /###\\", "/#####\\" },
+    { border = "none", zindex = 100, row = vim.o.lines - vim.o.cmdheight - 4 }
+  )
+  vim.wo[cannon_win].winhighlight =
+    "NormalFloat:KillerCannon,FloatBorder:KillerCannon"
+
+  local bullet_win
+  _, bullet_win = open_float(
+    { "|", "|" },
+    { border = "none", zindex = 80, row = vim.o.lines - vim.o.cmdheight - 5 }
+  )
+  vim.wo[bullet_win].winhighlight =
+    "NormalFloat:KillerBullet,FloatBorder:KillerBullet"
+
+  local sheep_win
+  _, sheep_win = open_float(SHEEP_SPRITES[1], { border = "none", zindex = 90 })
+  vim.wo[sheep_win].winhighlight =
+    "NormalFloat:KillerSheep,FloatBorder:KillerSheep"
+
+  local function close()
+    api.nvim_win_close(level_win, true)
+    api.nvim_win_close(cannon_win, true)
+    api.nvim_win_close(bullet_win, true)
+    api.nvim_win_close(sheep_win, true)
+  end
 end
 
 local function countdown()
   local blink_timer, sound_timer = loop.new_timer(), loop.new_timer()
-  local close_timer, _, win, augroup
+  local close_timer, win, augroup, _
 
   local function close()
     close_timer:stop()
@@ -126,7 +227,7 @@ local function countdown()
     sound_timer:stop()
     api.nvim_del_augroup_by_id(augroup)
     api.nvim_win_close(win, true)
-    round(1)
+    level(1)
   end
   _, win, augroup = open_float({
     "",
@@ -211,7 +312,7 @@ local function intro()
       if api.nvim_buf_is_valid(buf) then
         hl_extmark = api.nvim_buf_set_extmark(buf, ns, 1, hl_ranges[i][1], {
           id = hl_extmark,
-          hl_group = "IntroHl",
+          hl_group = "introHl",
           end_col = hl_ranges[i][2],
         })
         i = 1 + (i % #hl_ranges)
